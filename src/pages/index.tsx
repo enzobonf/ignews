329d9e0620324c2581073397cfa081f168bd7843
enzobonf <enzobonfx@gmail.com>
2022-10-23 19:27:50 -0300

    feat: adicionando fonte externa (Google Fonts)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  return <h1 className={styles.title}>salve</h1>;
+  return <h1 className={styles.title}>salveeee</h1>;
 }
