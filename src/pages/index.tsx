7f95e04a2285dbf0efcacb113e3baea3f1bc4e92
enzobonf <enzobonfx@gmail.com>
2022-10-23 19:35:30 -0300

    feat: titulo dinamico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
+import Head from 'next/head';
 import React from 'react';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
-  return <h1 className={styles.title}>salveeee</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>salveeee</h1>
+    </>
+  );
 }
