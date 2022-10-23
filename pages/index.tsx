d2fd974d3aef143fbf46a5775f01f56aeba1d586
enzobonf <enzobonfx@gmail.com>
2022-10-23 19:05:19 -0300

    add sass 2

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import React from 'react';
-
-export default function Home() {
-  const aaaa = 'ok';
-
-  return <h1>salve</h1>;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return <h1 className={styles.title}>salve</h1>;
+}
