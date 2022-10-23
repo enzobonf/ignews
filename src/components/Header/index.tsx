e5306a1ceaa928b179c96ce0d1454049058b94dd
enzobonf <enzobonfx@gmail.com>
2022-10-23 20:54:41 -0300

    feat: inicio SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image';
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
-
-console.log(styles);
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="" width={100} height={100} />
+
         <nav>
           <a className={styles.active} href="https://google.com">
             Home
           </a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
