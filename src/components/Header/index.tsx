68b02a8c8d75cd4f4ed46c9b438eeea232e79645
enzobonf <enzobonfx@gmail.com>
2022-10-25 20:24:03 -0300

    feat: página Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="" width={100} height={100} />
+        <Image src="/images/logo.svg" alt="ig.news" width={110} height={31} />
 
         <nav>
           <a className={styles.active} href="https://google.com">
