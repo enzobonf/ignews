097b1d5718b38d3029df05c2c49efe9f59a1cbf4
enzobonf <enzobonfx@gmail.com>
2022-10-25 19:56:08 -0300

    feat: finalização SignInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
-export function SignInButton() {
-  return (
-    <button type="button">
-      <FaGithub />
+interface Props {
+  isUserLoggedIn?: boolean;
+}
+
+export function SignInButton({ isUserLoggedIn = true }: Props) {
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Enzo Bonfante
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
   );
